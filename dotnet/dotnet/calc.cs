fa7aa0dff02913939bb4054d73e27a7f4e8d8a94
marcinau <stefek-zenonowaty@wp.pl>
2019-03-06 14:29:06 +0100

    zmieniam sobie

---
+++ dotnet/dotnet/calc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet
+{
+    public class calc
+    {
+        public decimal Divide(decimal divided, int divisor)
+        {
+            throw new NotImplementedException();
+            heheheheheeh
+        }
+    }
+}
